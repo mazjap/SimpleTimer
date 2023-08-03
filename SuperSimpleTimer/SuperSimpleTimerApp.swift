beb1fe0d416c21e2fd74053302015dae496747a7
Jordan Christensen <jordan.c4922@gmail.com>
2023-08-02 23:38:21 -0600

    Initial Commit

---
+++ SuperSimpleTimer/SuperSimpleTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSimpleTimerApp.swift
+//  SuperSimpleTimer
+//
+//  Created by Jordan Christensen on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SuperSimpleTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
